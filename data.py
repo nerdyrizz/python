dc1461e563ecef388f916280beec21bd115b5561
nerdyrizz <nagpalsmaira@gmail.com>
2025-07-22 18:50:46 +0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,8 @@
+a = 5
+print("data type of a:", type(a) )
+b = 13.4
+print("data type of b:" , type(b))
+c = True
+print("data type of c:", type(c))
+d = "Smaira"
+print("data type of d:", type(d)) 
