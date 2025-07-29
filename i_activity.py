4cdf89782614c11696a063ff7d16f61726e3cf4a
nerdyrizz <nagpalsmaira@gmail.com>
2025-07-29 18:34:21 +0400

    Add files via upload

---
+++ i_activity.py
@@ -0,0 +1,8 @@
+i = int(input("Enter an integer: "))
+if (i > 30):
+    print("The integer is greater than 30.")
+    print("I am in the if block.")
+else:
+    print("The integer is not greater than 30.")
+    print("I am in the else block.")
+print("I am outside the if-else block.")
