9922cb28e5d6ec05c3aedb6b37304f28a6d838c5
nerdyrizz <nagpalsmaira@gmail.com>
2025-08-02 10:40:16 +0400

    Add files via upload

---
+++ AND_OR.py
@@ -0,0 +1,22 @@
+a = 10
+b = 12
+c = 0
+if a and b and c:
+    print("All the numbers have Boolean value as true.")
+else:
+    print("At least one of the numbers has a Boolean value as false.")
+
+
+a = 10
+b = -10
+c = c
+
+if a>10 or b>10:
+    print("At least one of the numbers is greater than 10.")
+else:
+    print("No number is greater than 10.")
+
+if b > 10 or c > 10:
+    print("At least one of the numbers is greater than 10.")
+else:
+    print("No number is greater than 10.")
