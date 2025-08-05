7898200712c84197754edd91b9825329bbeaec81
nerdyrizz <nagpalsmaira@gmail.com>
2025-08-05 17:53:34 +0400

    Add files via upload

---
+++ bday.py
@@ -0,0 +1,11 @@
+jack_bday = "2010-10-15"
+aarav_bday = "2010-04-16"
+sara_bday = "2010-12-22"
+mike_bday = "2010-09-05"
+leo_bday = "2009-02-03"
+print("Jack's birthday is on", jack_bday)
+print("Aarav's birthday is on", aarav_bday) 
+print("Sara's birthday is on", sara_bday)
+print("Mike's birthday is on", mike_bday)
+print("Leo's birthday is on", leo_bday)
+
