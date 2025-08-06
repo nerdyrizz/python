e0fb2d1caf0e3c0860e86f1d6089895aad82f40e
nerdyrizz <nagpalsmaira@gmail.com>
2025-08-06 21:11:22 +0400

    Add files via upload

---
+++ agechecker.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age:"))
+if age > 10:
+    if age < 20:
+        print("Your age is between 10 and 20.")
+    else:
+        print("Your age is above 20.")
+else:
+    print("Your age is 10 or below.")
