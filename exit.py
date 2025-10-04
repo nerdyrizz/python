49995bacbfd0e508dc48548d4df3212ecc7d742b
nerdyrizz <nagpalsmaira@gmail.com>
2025-10-04 16:18:27 +0400

    Add files via upload

---
+++ exit.py
@@ -0,0 +1,8 @@
+# python program to demonstrate exit()
+
+for i in range(10):
+    # if i becomes 5, then the program is forced to exit
+    if i == 5:
+        print(exit)
+        exit()
+    print(i)
