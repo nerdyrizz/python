b2cf58692b703c6c756cd957d955d4472d558bed
nerdyrizz <nagpalsmaira@gmail.com>
2025-10-12 18:52:50 +0400

    Add files via upload

---
+++ password.py
@@ -0,0 +1,7 @@
+import random
+import string
+
+characters = string.ascii_letters + string.digits 
+
+password = ''.join(random.choice(characters) for i in range(12))
+print("Your new password is: ", password)
