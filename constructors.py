372ae6d0b4b89e2571d7bd171037d84d5212bd3e
nerdyrizz <nagpalsmaira@gmail.com>
2025-10-18 16:07:25 +0400

    Add files via upload

---
+++ constructors.py
@@ -0,0 +1,21 @@
+# parent class
+class Person(object):
+    def __init__ (self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+
+# child class
+class Employee(Person):
+    def __init__ (self, name, idnumber, salary, post):
+        self.salary = salary
+        self.post = post
+
+        # invoking the __init__ of the parent class
+        Person.__init__(self, name, idnumber)
+
+a = Employee('Rahul', 676767, 30000, "Intern")
+a.display()
