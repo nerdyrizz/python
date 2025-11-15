22af84e0dbb1ee8db0ca66ab8eddfb4d1530d5fb
nerdyrizz <nagpalsmaira@gmail.com>
2025-11-15 16:23:54 +0400

    Add files via upload

---
+++ space_invaders.py
@@ -0,0 +1,83 @@
+import math
+import random
+import pygame
+
+# constants
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 500
+PLAYER_START_X =370
+PLAYER_START_Y=380
+ENEMY_START_Y_MIN = 50
+ENEMY_START_Y_MAX = 150
+ENEMY_SPEED_X = 4
+ENEMY_SPEED_Y = 40
+BULLET_SPEED_Y = 10
+COLLISION_DISTANCE = 27
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Space Invaders")
+Icon = pygame.image.load("ufo.png")
+pygame.display.set_icon(Icon)
+background = pygame.image.load("background.png")
+
+# player
+playerImg = pygame.image.load("player.png")
+playerX = PLAYER_START_X
+playerY = PLAYER_START_Y
+playerX_change = 0
+
+# enemys
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load("enemy.png"))
+    enemyX.append(random.randint(0, SCREEN_WIDTH -  64)) # 64 IS THE SIZE OF THE ENEMY IMAGE
+    enemyY.append(random.randint(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX))
+    enemyX_change.append(ENEMY_SPEED_X)
+    enemyY_change.append(ENEMY_SPEED_Y)
+
+# bullet
+bulletImg = pygame.image.load("bullet.png")
+bulletX = 0
+bulletY = PLAYER_START_Y
+bulletY_change = BULLET_SPEED_Y
+bullet_state = "ready"  # "ready" - You can't see the bullet on the screen, "fire" - The bullet is currently moving
+
+#Score
+score_value = 0
+font = pygame.font.Font("freesansbold.ttf", 32)
+textX = 10
+testY = 10
+
+# game over text
+over_font = pygame.font.Font("freesansbold.ttf", 64)
+
+def show_score(x,y):
+    score = font.render("Score : " + str(score_value), True, (255,255,255))
+    screen.blit(score, (x,y))
+
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (255,255,255))
+    screen.blit(over_text, (200,250))
+
+def player(x,y):
+    screen.blit(playerImg, (x,y))
+
+def enemy(x,y,i):
+    screen.blit(enemyImg[i], (x,y) )
+
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
+
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt((enemyX - bulletX)**2 + (enemyY - bulletY)**2)
+    if distance < COLLISION_DISTANCE:
+        return True
+    else:
+        return False
